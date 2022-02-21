4266fcbdcb44621d5e337e1b0da7ebc1290cb983
Lee4339 <skyho4339@naver.com>
2022-02-21 11:17:10 +0900

    연산자 수업내용

---
+++ JavaStudy_4084/src/a03_연산자/Operation01.java
@@ -0,0 +1,17 @@
+package a03_연산자;
+
+public class Operation01 {
+
+	public static void main(String[] args) {
+		// 증(가)감(소)연산자
+		int score = 10;
+		// score = score + 1;
+		// 앞에 ++(선증가-선감소), 뒤에++(후증가-후감소)
+		//System.out.println(++score); -> 바로 증가
+		System.out.println(score++); // 현 문장에서 증가나 감소하지는 않으나 다음score의 사용 때 증가나 감소해 있는 상태
+		System.out.println(score--);
+		System.out.println(score);
+
+	}
+
+}
