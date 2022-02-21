6f04153fd533094ee195ed368603c1ccb1160e6a
Lee4339 <skyho4339@naver.com>
2022-02-21 12:25:59 +0900

    연산자 수업내용 추가

--- JavaStudy_4084/src/a03_연산자/Operation01.java
+++ JavaStudy_4084/src/a03_연산자/Operation01.java
@@ -8,8 +8,9 @@ public class Operation01 {
 		// score = score + 1;
 		// 앞에 ++(선증가-선감소), 뒤에++(후증가-후감소)
 		//System.out.println(++score); -> 바로 증가
-		System.out.println(score++); // 현 문장에서 증가나 감소하지는 않으나 다음score의 사용 때 증가나 감소해 있는 상태
-		System.out.println(score--);
+		System.out.println(score--); // 현 문장에서 증가나 감소하지는 않으나 다음score의 사용 때 증가나 감소해 있는 상태
+		System.out.println(score);
+		System.out.println(score++);
 		System.out.println(score);
 
 	}
