68e88d28d600a56cf36eb2c9e5b33a798115b936
lee4339 <skyho4339@naver.com>
2022-03-02 12:55:25 +0900

    클래스 정보은닉 수업내용

---
+++ JavaStudy_4084/src/a09_클래스_정보은닉/AccessModifierTest.java
@@ -0,0 +1,19 @@
+package a09_클래스_정보은닉;
+//https://studymake.tistory.com/424 참고
+
+import a09_클래스.Student;
+
+public class AccessModifierTest {
+
+	public static void main(String[] args) {
+		AccessModifier accessModifier = new AccessModifier();
+//		accessModifier.name = "이건호";
+//		accessModifier.age = 29;
+//		
+//		Student student = new Student();
+		
+		
+
+	}
+
+}
