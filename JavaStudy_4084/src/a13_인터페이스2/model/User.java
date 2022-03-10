f3c1e3136302fd392abcef9397458af0521b6369
lee4339 <skyho4339@naver.com>
2022-03-10 18:53:47 +0900

    인터페이스2 수업내용

--- JavaStudy_4084/src/a13_인터페이스2/model/User.java
+++ JavaStudy_4084/src/a13_인터페이스2/model/User.java
@@ -39,9 +39,12 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public void showUser() {
-		System.out.println("User[username = " + username + ", passwor =  " + password + ", name =  " + name + ", email =  " + email + "]");
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
+	
+	
 
 }
